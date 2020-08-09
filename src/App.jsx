2a6f50cd81a2f4c43656ada612a566503a49a49a
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-08-09 19:51:00 +0400

    added functionality to select boxes

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,49 @@ import Dropdown from "./Dropdown/Dropdown";
 import styles from "./styles.scss";
 
 const monthsArr = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  {
+    January: "01",
+  },
+  {
+    February: "02",
+  },
+  {
+    March: "03",
+  },
+  {
+    April: "04",
+  },
+  {
+    May: "05",
+  },
+  {
+    June: "06",
+  },
+  {
+    July: "07",
+  },
+  {
+    August: "08",
+  },
+  {
+    September: "09",
+  },
+  {
+    October: "10",
+  },
+  {
+    November: "11",
+  },
+  {
+    December: "12",
+  },
 ];
+
+const justMonthsNamesArr = monthsArr.map((it) => Object.keys(it)[0]);
 
 const App = () => {
   const [events, setEvents] = useState([]);
+  const [selectedOption, changeSelectedOption] = useState("");
   const url =
     "https://raw.githubusercontent.com/xsolla/xsolla-frontend-school-2020/master/events.json";
   useEffect(() => {
@@ -28,17 +55,39 @@ const App = () => {
       .then((res) => res.json())
       .then((res) => setEvents(res));
   }, [url]);
+  const onOptionChange = (option) => {
+    changeSelectedOption(option);
+  };
   const citiesArr = [...new Set(events.map((event) => event.city))];
+  const isMonthSelected = justMonthsNamesArr.indexOf(selectedOption) !== -1;
+  let filterdEvents;
+  if (isMonthSelected) {
+    const month = monthsArr.filter((it) => it[selectedOption])[0][
+      selectedOption
+    ];
+    filterdEvents = events.filter((event) => event.date.slice(3, 5) === month);
+  } else {
+    filterdEvents = events.filter((event) => event.city === selectedOption);
+  }
+  const dataToPass = selectedOption === "" ? events : filterdEvents;
 
   return (
     <div className={styles.outerContainer}>
       <h1 className={styles.heading}>Event Listing</h1>
       <div className={styles.select}>
-        <Dropdown title="City" options={citiesArr} />
-        <Dropdown title="Month" options={monthsArr} />
+        <Dropdown
+          title="City"
+          options={citiesArr}
+          onOptionChange={onOptionChange}
+        />
+        <Dropdown
+          title="Month"
+          options={justMonthsNamesArr}
+          onOptionChange={onOptionChange}
+        />
       </div>
       <div className={styles.cardFolder}>
-        {events.map((event) => (
+        {dataToPass.map((event) => (
           <Card key={event.id} {...event} />
         ))}
       </div>
--- src/Dropdown/Dropdown.jsx
+++ src/Dropdown/Dropdown.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Dropdown.styles.scss";
 
-const Dropdown = ({ title, options }) => (
-  <div className={styles.container}>
-    <p className={styles.title}>{title}:</p>
-    <select className={styles.selectContainer}>
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
-  </div>
-);
+const Dropdown = ({ title, options, onOptionChange }) => {
+  const [month, setMonth] = useState("");
+  const handleChange = (e) => {
+    setMonth(e.target.value);
+    onOptionChange(e.target.value);
+  };
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>{title}:</p>
+      <select
+        className={styles.selectContainer}
+        value={month}
+        onChange={handleChange}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default Dropdown;
