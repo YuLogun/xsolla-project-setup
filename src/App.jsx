6c2c54fc2140e64843bf5c3971e14872ec90eb65
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-08-18 16:06:15 +0400

    deleted switch from the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import CardInfo from "./CardInfo/CardInfo";
 
@@ -9,17 +9,15 @@ const About = () => <h2>About</h2>;
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Layout />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/:eventId">
-          <CardInfo />
-        </Route>
-      </Switch>
+      <Route exact path="/">
+        <Layout />
+      </Route>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/:eventId">
+        <CardInfo />
+      </Route>
     </BrowserRouter>
   );
 };
