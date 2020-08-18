36f58549158005b185f11f656ab8063ae2a1599a
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-08-18 16:13:26 +0400

    put the switch back

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import CardInfo from "./CardInfo/CardInfo";
 
@@ -9,15 +9,17 @@ const About = () => <h2>About</h2>;
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/">
-        <Layout />
-      </Route>
-      <Route exact path="/about">
-        <About />
-      </Route>
-      <Route exact path="/:eventId">
-        <CardInfo />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Layout />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/:eventId">
+          <CardInfo />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 };
